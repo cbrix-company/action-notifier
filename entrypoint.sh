bfa14103d85fcd18ac1a1af2349b743b50f54623
Lockwood <lockwooddev@gmail.com>
2021-06-14 16:54:33 +0300

    run main from root

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -l
-python main.py --slack-channel-id $2 --slack-token $1 --output-file $3 --repo-name $4
+python /main.py --slack-channel-id $2 --slack-token $1 --output-file $3 --repo-name $4
