f4e4f126a546ef9cc9df3e77eefd5b83cebe516a
Lockwood <lockwooddev@gmail.com>
2021-06-23 16:25:53 +0300

    missing dollar for variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 msg='"$4"'
-python /main.py --message-type $1 --slack-token $2 --slack-channel-id $3 --message msg
+python /main.py --message-type $1 --slack-token $2 --slack-channel-id $3 --message $msg
