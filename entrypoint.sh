13040e4ed571de5c7366ebd44cde3eb9be80893e
Lockwood <lockwooddev@gmail.com>
2021-06-23 16:15:19 +0300

    double quote argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh -l
-python /main.py --message-type $1 --slack-token $2 --slack-channel-id $3 --message "'"$4"'"
+python /main.py --message-type $1 --slack-token $2 --slack-channel-id $3 --message "$4"
