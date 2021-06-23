083eea2b9bec9a63081adbeae384034bf4f69906
Lockwood <lockwooddev@gmail.com>
2021-06-23 16:23:01 +0300

    evaluate arg in single quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,3 @@
 #!/bin/sh -l
-python /main.py --message-type $1 --slack-token $2 --slack-channel-id $3 --message "$4"
+msg='"$4"'
+python /main.py --message-type $1 --slack-token $2 --slack-channel-id $3 --message msg
